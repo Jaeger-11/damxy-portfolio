d4e0b854ea900a1e6030714fc2a49abb363affde
Oluwadamilola Falodun <dpaulzs11@gmail.com>
2024-05-04 02:32:29 +0100

    ...

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,23 +7,18 @@ const Experience = () => {
             <div className='flex justify-center'>
                 <h3 className="p-3 my-4 sh font-bold rotate-0 text-white bg-darkBlue w-max"> My Experience</h3>
             </div>
-            <ul className='p-4 flex flex-col gap-3'>
+            <ul className='p-4 flex flex-col gap-3 text-lg'>
                 <li >
-                    <h3 className='font-semibold text-lg'>Gathpay (January 2023 - April 2023)</h3>
-                    <p className='font-medium italic text-lg'>Frontend Intern</p>
-                    {/* <div>
-                        <p>
-                        <svg viewBox="0 0 15 15" className='w' fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10.2071 7.49995L6 3.29285V11.7071L10.2071 7.49995Z" fill="#000000"></path> </g></svg>
-                        </p>
-                    </div> */}
+                    <h3 className='font-semibold'> Gathpay (January 2023 - April 2023)</h3>
+                    <p className='font-medium'>Frontend Intern</p>
                 </li>
                 <li>
-                    <h3 className='font-semibold text-lg'>Freelance (April 2023 - January 2024)</h3>
-                    <p className='font-medium italic text-lg'>UI Developer</p>
+                    <h3 className='font-semibold'>Freelance (April 2023 - January 2024)</h3>
+                    <p className='font-medium'>UI Developer</p>
                 </li>
                 <li>
-                    <h3 className='font-semibold text-lg'>Aggregate Business Solutions Ltd (January 2024 - Present)</h3>
-                    <p className='font-medium italic text-lg'>Frontend Developer (NYSC) </p>
+                    <h3 className='font-semibold'>Aggregate Business Solutions Ltd (January 2024 - Present)</h3>
+                    <p className='font-medium'>Frontend Developer (NYSC) </p>
                 </li>
             </ul>
         </article>
